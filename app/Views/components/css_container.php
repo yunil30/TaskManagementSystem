e84e72af967351073e89117011be45acab40f456
ucdungo <yunildungo@gmail.com>
2025-06-25 08:59:43 +0800

    06-25-2025

--- app/Views/components/css_container.php
+++ app/Views/components/css_container.php
@@ -551,7 +551,6 @@
 
                 &:focus {
                     border-color: #ff522d;
-                    box-shadow: 0px 0px 3px #ff522d;
                     font-size: 13px;
                     letter-spacing: .5px;
                     color: #1f2328;
