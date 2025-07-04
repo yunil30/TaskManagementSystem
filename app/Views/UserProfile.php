48b111071e336048c834e8ca35c05afda1f236b1
yunil30 <yunildungo@gmail.com>
2025-07-04 15:35:04 +0800

    07-04-2025

--- app/Views/UserProfile.php
+++ app/Views/UserProfile.php
@@ -59,7 +59,7 @@
                 justify-content: center;
                 background-color: white;
                 color: #1f2328;
-                box-shadow: 0px 1px 5px #00000035;
+                box-shadow: 0px 0px 1px #00000035;
                 border: 1px solid rgb(215, 215, 215);
                 border-radius: 50px;
                 padding: 5px 18px;
